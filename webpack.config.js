073cf0800456804308894ec1a5acacd16ac406f9
khvorosttt <khvorosttt@yandex.by>
2024-05-02 11:27:48 +0300

    feat: remove airbnb, add sass and change padding

--- webpack.config.js
+++ webpack.config.js
@@ -6,56 +6,60 @@ const CopyPlugin = require('copy-webpack-plugin');
 const EslintPlugin = require('eslint-webpack-plugin');
 
 const commonConfig = {
-    entry: path.resolve(__dirname, './src/index.ts'),
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource',
-            },
-            {
-                test: /\.(mp3|ogg)$/,
-                loader: 'file-loader',
-            },
-            {
-                test: /\.ts$/i,
-                use: 'ts-loader',
-            },
-            {
-                test: /\.json$/,
-                use: ['json-loader'],
-                type: 'javascript/auto',
-            },
-        ],
-    },
-    resolve: {
-        extensions: ['.ts', '.js'],
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'index_bundle.js',
-    },
-    devServer: {
-        open: true,
-        host: 'localhost',
-        historyApiFallback: true,
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            title: 'eCommerce-Application',
-        }),
-        new EslintPlugin({ extensions: ['ts'] }),
+  entry: path.resolve(__dirname, './src/index.ts'),
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(mp3|ogg)$/,
+        loader: 'file-loader',
+      },
+      {
+        test: /\.ts$/i,
+        use: 'ts-loader',
+      },
+      {
+        test: /\.json$/,
+        use: ['json-loader'],
+        type: 'javascript/auto',
+      },
     ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index_bundle.js',
+  },
+  devServer: {
+    open: true,
+    host: 'localhost',
+    historyApiFallback: true,
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'eCommerce-Application',
+    }),
+    new EslintPlugin({ extensions: ['ts'] }),
+  ],
 };
 
 module.exports = ({ mode }) => {
-    const isProductionMode = mode === 'prod';
-    const envConfig = isProductionMode ? require('./webpack.prod.config') : require('./webpack.dev.config');
+  const isProductionMode = mode === 'prod';
+  const envConfig = isProductionMode ? require('./webpack.prod.config') : require('./webpack.dev.config');
 
-    return merge(commonConfig, envConfig);
+  return merge(commonConfig, envConfig);
 };
