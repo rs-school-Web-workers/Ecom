c4488501dd2fb2f624885f1af4b53c6fb1dd2f8b
Kirill <kiw197204@gmail.com>
2024-05-13 13:11:22 +0300

    feat: sign in done

--- src/index.ts
+++ src/index.ts
@@ -2,27 +2,7 @@ import { test } from './test';
 import { getApiRoot } from './utils/api/Client';
 
 console.log('index.ts', test);
-getApiRoot('asd@asd.asd', 'asd')
-  .orders()
-  .get()
-  .execute()
-  .then(({ body }) => {
-    console.log(JSON.stringify(body));
-  })
-  .catch(console.error);
+getApiRoot('asd@asd.asd', 'asd').me().orders().get().execute().catch(console.error);
+getApiRoot().get().execute().then().catch(console.error);
 
-/* getApiRoot()
-  .customers()
-  .post({
-    body: {
-      email: 'johndoe@example.com',
-      firstName: 'John',
-      lastName: 'Doe',
-      password: 'secret123',
-    },
-  })
-  .execute()
-  .then(({ body }) => {
-    console.log(JSON.stringify(body));
-  })
-  .catch(console.error); */
+//signIn('asd1@asd.asd', 'test', 'testov', 'asd');
--- src/utils/api/Client.ts
+++ src/utils/api/Client.ts
@@ -2,17 +2,17 @@ import { ByProjectKeyRequestBuilder, createApiBuilderFromCtpClient } from '@comm
 import { ClientBuilder } from '@commercetools/sdk-client-v2';
 
 let inst: ByProjectKeyRequestBuilder | null = null;
+const projectKey = process.env.PROJECT_KEY;
+const region = process.env.REGION;
+const clientId = process.env.CLIENT_ID;
+const clientSecret = process.env.CLIENT_SECRET;
+const clientScopes = process.env.CLIENT_SCOPES;
+const scopes = clientScopes?.split(' ');
 
 export function getApiRoot(username?: string, password?: string) {
-  const projectKey = process.env.PROJECT_KEY;
-  const region = process.env.REGION;
-  const clientId = process.env.CLIENT_ID;
-  const clientSecret = process.env.CLIENT_SECRET;
-  const clientScopes = process.env.CLIENT_SCOPES;
   if (!projectKey || !region || !clientId || !clientSecret || !clientScopes) {
     throw new Error('Env parameters are undefined');
   }
-  const scopes = clientScopes.split(' ');
   if (!inst) {
     if (!username || !password) throw new Error('Empty credentials');
     let ctpClient = new ClientBuilder()
@@ -50,4 +50,45 @@ export function destroyApiRoot() {
 export function isLogged() {
   if (inst) return true;
   return false;
+}
+
+export function signIn(email: string, firstName: string, lastName: string, password: string) {
+  if (!projectKey || !region || !clientId || !clientSecret || !clientScopes) {
+    throw new Error('Env parameters are undefined');
+  }
+  let ctpClient = new ClientBuilder()
+    .withClientCredentialsFlow({
+      host: `https://auth.${region}.gcp.commercetools.com`,
+      projectKey,
+      credentials: {
+        clientId,
+        clientSecret,
+      },
+      scopes,
+      fetch,
+    })
+    .withHttpMiddleware({
+      host: `https://api.${region}.gcp.commercetools.com`,
+      fetch,
+    });
+  if (process.env.NODE_ENV === 'development') {
+    ctpClient = ctpClient.withLoggerMiddleware();
+  }
+
+  createApiBuilderFromCtpClient(ctpClient.build())
+    .withProjectKey({ projectKey })
+    .customers()
+    .post({
+      body: {
+        email,
+        firstName,
+        lastName,
+        password,
+      },
+    })
+    .execute()
+    .then(() => {
+      destroyApiRoot();
+      getApiRoot(email, password);
+    });
 }
