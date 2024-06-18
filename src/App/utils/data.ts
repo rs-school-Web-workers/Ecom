48556fa738778011e2f050756256546b8e8a9df2
Danila <AngelofDest1ny@yandex.ru>
2024-06-18 11:51:55 +0300

    feat: add shortBio at AboutUs Page

--- src/App/utils/data.ts
+++ src/App/utils/data.ts
@@ -15,14 +15,14 @@ export const data: DataAboutUs[] = [
     img: KirillImage,
     name: 'Kirill',
     role: 'Front-end developer',
-    shortBio: '',
+    shortBio: `Hello! My name is Kiryl. This year I graduated from BSUIR. I learned php online and I have a small work expirience on inner project of bsuir lab.  Currently I’m employed as sysadmin in ministry of finance. And I’m actively looking for a new job as developer. My main focus is on becoming a fullstack developer, because I’m interested in web development at whole. About me as a human being. I like discovering new things, thats why I chose riding a bicycle as my major hobby. When its too cold to ride a bicycle I prefer swimming. Sometimes I like to play big tennis with my little cousin at Minsk arena. As the way to switch my intellectual activity I learn how to make electronic music.`,
     ghLink: 'https://github.com/Salt-Upon-Wounds',
   },
   {
     img: TatianaImage,
     name: 'Tatiana',
     role: 'Front-end developer',
-    shortBio: '',
+    shortBio: `Hello, my name is Tatiana. I graduated with a bachelor's degree in applied mathematics and have a master's degree in physical and mathematical sciences. I live in Brest, I adore books and devote almost all my free time to reading, sing, draw and try to enjoy everything I do. While studying at the university, I learned the basics of front-end, and after learning about Rolling Scope School, I decided to challenge myself and consolidate my existing knowledge and gain new ones. Over the course of these 8 months, I have made an amazing journey and gained knowledge that I will undoubtedly try to put into practice. Thank you for the opportunity to be a part of RSShool.`,
     ghLink: 'https://github.com/khvorosttt',
   },
 ];
